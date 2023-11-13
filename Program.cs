4c11d56c012897f8c94d0e50751b95442702a4b8
Jason <jason5donoghue@gmail.com>
2023-11-13 21:42:48 +0000

    first cdoe ascii art

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            // This program is to practice running C# and design some Ascii art
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| 0 |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Poppy");
+        }
+    }
+}
